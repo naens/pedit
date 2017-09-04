8e15dfd59a6ce8aba437b641e08cc5ca225ea17c
naens <naens@gmx.com>
2017-09-04 15:49:28 +0200

    pedit-racket: add text-cell gui component class

---
+++ pedit-racket/gui-classes/text-cell.rkt
@@ -0,0 +1,32 @@
+#lang racket/gui
+
+(define text-cell%
+  (class horizontal-panel%
+    (init pre text post)
+    (super-new)
+
+    (send this border 1)
+
+    (define current-pre pre)
+    (define current-text text)
+    (define current-post post)
+
+    (define/public (get-pre) current-pre)
+    (define/public (get-text) current-text)
+    (define/public (get-post) current-post)
+
+    (define pre-label (new message% (parent this) (label pre)))
+    (define text-label (new message% (parent this) (label text)))
+    (define post-label (new message% (parent this) (label post)))
+
+    (define/public (set-pre new-pre)
+      (set! current-pre new-pre)
+      (send pre-label set-label new-pre))
+
+    (define/public (set-text new-text)
+      (set! current-text new-text)
+      (send text-label set-label new-text))
+
+    (define/public (set-post new-post)
+      (set! current-post new-post)
+      (send post-label set-label new-post))))
