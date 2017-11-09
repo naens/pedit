bf054e3573afc17297e6f8615a738752675f4af0
naens <naens@gmx.com>
2017-11-09 11:52:25 +0100

    pedit:pedit-racket: set cell text function

--- pedit-racket/database/db-text-cell.rkt
+++ pedit-racket/database/db-text-cell.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require db)
+(require "db-common.rkt")
 
 (provide db-text-cell-get db-text-cell-set)
 
@@ -11,35 +12,27 @@
         (values (vector-ref row 0) (apply string-append text) (vector-ref row 1))))))
  
 (define (db-text-cell-set db tv-id node-id pre text post)
-  ;; set pre/post to cell
-  (let ((row (query-maybe-row db "select Pre, Post from TextCell where TextVersionID=$1 and TextNodeID=$2;" tv-id node-id)))
+  ;; 1. if there is already a text-cell for given tv-id/node-id pair: create new text-cell, otherwise: update existing
+  (let ((row (query-maybe-row db "select * from TextCell where TextVersionID=$1 and TextNodeID=$2;" tv-id node-id)))
     (if row
         (query-exec db "update TextCell set Pre=$1, Post=$2 where TextVersionID=$1 and TextNodeID=$2;" pre post tv-id node-id)
         (query-exec db "insert into TextCell (TextVersionID, TextNodeID, Pre, Post) values ($1, $2, $3, $4);" tv-id node-id pre post)))
-    ;; TODO: if wp exists, ???
-             otherwise: create new wp
-;;    ;; check if wp (tv-id/nodde-id) exists, is so delete its words (!
-;;    ;; delete word parts in other cell containing parts of words to be
-;;    ;; deleted
-;;    (db-text-cell-create-wp db tv-id node-id text)
 
-;; PREVIOUS VERSION
-;;    (let ((ti-id (memf (lambda (id)
-;;                         (equal? (db-text-item-get-string db id) text))
-;;                       tis)))
-;;      (print (format "{ti-id=~a}~%" ti-id))
-;;      (if ti-id
-;;          (db-connect-ti-tv db tv-id ti-id)           ; if found: connect tv to this text item
-;;          (db-text-item-create db tv-id node-id text)))  ; if not found: create new and connect
-;;         ))
+  ;; 2. if there is a wp for given tv-id/node-id: delete it (+ also words and word-parts associated with it)
+  (let ((words (query-list db "select WordID from WordPart where TextVersionID=$1 and TextNodeID=$2;" tv-id node-id)))
+    (map (lambda (word-id)
+           (query-exec db "delete from Word where WordID=$1;" word-id))
+         words))
 
-))
+  ;; 3. create new wp with a single word containing text
+  (db-text-cell-create-wp db tv-id node-id text)
 
+  ;; 4. TODO: what if user wants to edit a single word part?
+)
 
 (define (db-text-cell-get-string db tv-id node-id)
   (let ((wps (query-list db "select Text from WordPart where TextVersionID=$1 and TextNodeID=$2 order by TI_O;" tv-id node-id)))
     (list->string wps)))
-
 
 (define (db-text-cell-create-wp db tv-id node-id text)
   (define word-id (db-last-id db (query db "insert into Word values (NULL);")))
