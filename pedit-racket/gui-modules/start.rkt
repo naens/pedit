2f1853f115ddc0efe728aeea74ff9ae7bc3e6cf2
naens <naens@gmx.com>
2017-12-23 00:21:39 +0100

    Update start.rkt

--- pedit-racket/gui-modules/start.rkt
+++ pedit-racket/gui-modules/start.rkt
@@ -9,8 +9,8 @@
 ; Make a frame by instantiating the frame% class
 (define frame (new frame% (label "Pedit - Database Init Module")))
 
-(new message% (parent frame)
-     (label "Pedit: Database Init Module"))
+(define msg1 (new message% (parent frame)
+                  (label "Pedit: Database Init Module")))
 
 ;; Text Panel
 (define db-panel (new horizontal-panel% (parent frame)))
@@ -20,28 +20,28 @@
                          (label "Database File")
                          (init-value "tmp.db")))
 
-(new button% (parent db-panel)
-             (label "Get-File")
-             (callback (lambda (button event)
-                         (let ((fn (get-file)))
-                           (when fn
-                               (send db-text-field set-value (path->string fn)))))))
+(define btn1 (new button% (parent db-panel)
+                  (label "Get-File")
+                  (callback (lambda (button event)
+                              (let ((fn (get-file)))
+                                (when fn
+                                  (send db-text-field set-value (path->string fn))))))))
 
-(new button% (parent frame)
-             (label "Open/Create")
-             (callback (lambda (button event)
-                         (let* ((fp (string->path (send db-text-field get-value)))
-                                (db (if (file-exists? fp)
-                                       (connect-db fp)
-                                       (create-db fp))))
-                           (query-exec db "PRAGMA foreign_keys = ON")
-                           (show-text-module db)))))
+(define btn2 (new button% (parent frame)
+                  (label "Open/Create")
+                  (callback (lambda (button event)
+                              (let* ((fp (string->path (send db-text-field get-value)))
+                                     (db (if (file-exists? fp)
+                                             (connect-db fp)
+                                             (create-db fp))))
+                                (query-exec db "PRAGMA foreign_keys = ON")
+                                (show-text-module db))))))
 
 ;; Exit Button
-(new button% (parent frame)
-             (label "Exit")
-             (callback (lambda (button event)
-                         (send frame show #f))))
+(define btn3 (new button% (parent frame)
+                  (label "Exit")
+                  (callback (lambda (button event)
+                              (send frame show #f)))))
 
 ; Show the frame by calling its show method
 (send frame show #t)
